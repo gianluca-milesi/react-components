9c905afb41218a9fcff58df2b2a9ac5694bf3278
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-27 16:18:49 +0100

    ADD: Main and Card components

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+function Card() {
+    return (
+        <>
+            <div className="card">
+                <img src="" />
+                <div className="card-body">
+                    <h3 className="card-title">Titolo del post</h3>
+                    <p className="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quo magnam, officia eius qui modi.</p>
+                    <button>Componente Bottone</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/Card/Card.jsx"
+
+function Main() {
+    return (
+        <>
+            <div className="container">
+                <Card />
+            </div>
+        </>
+    )
+}
+
+export default Main
