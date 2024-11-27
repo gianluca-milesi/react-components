7bc3c4321693eab13159631c7a0246da38dd9834
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-27 17:21:16 +0100

    ADD: Button component. Style to Button and Card components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+import style from "./Button.module.css"
+
+function Button(){
+    return(
+        <button className={style.button}>Leggi di più</button>
+    )
+}
+
+export default Button
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,20 @@
-import image from "../../assets/random-image.jpg"
 import style from "./Card.module.css"
+import image from "../../assets/random-image.jpg"
+import Button from "../Button/Button.jsx"
 
 function Card() {
     return (
         <>
             <div className={style.card}>
-                <img src={image} />
+                <img className={style.image} src={image} />
                 <div className={style.body}>
-                    <h3 className="card-title">Titolo del post</h3>
-                    <p className="card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ratione soluta et ipsa dolores, autem perferendis non eius, ipsum esse nobis minima voluptas! Delectus, expedita?</p>
-                    <button>Componente Bottone</button>
+                    <div className={style.text}>
+                        <h2>Titolo del post</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ratione soluta et ipsa dolores, autem perferendis non eius, ipsum esse nobis minima voluptas! Delectus, expedita?</p>
+                    </div>
+                    <Button />
                 </div>
+
             </div>
         </>
     )
