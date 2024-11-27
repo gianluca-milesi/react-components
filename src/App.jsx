94ed381b2e31b9b90d937633b4217c4c6072a2b0
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-27 17:47:03 +0100

    ADD: Footer component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./layouts/Header/Header.jsx"
 import Main from "./layouts/Main.jsx"
+import Footer from "./layouts/Footer/Footer.jsx"
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import style from "./Footer.module.css"
+function Footer() {
+    return (
+        <footer>
+            <div className={style.info}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quo placeat reprehenderit nihil possimus animi laboriosam omnis consequatur voluptatibus velit, aliquam doloribus, a quam labore modi culpa eligendi saepe atque.</div>
+        </footer>
+    )
+}
+
+export default Footer
